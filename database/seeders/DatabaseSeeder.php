e12ca885170881125108303f3f76bb42b21b5cec
Taufit hidayat <49459014+taufit23@users.noreply.github.com>
2022-10-23 20:56:00 +0700

    add admin & dokter account 'seeding'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\Layanan;
+use App\Models\Admin;
+use App\Models\Dokter;
+use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,14 +21,26 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        DB::table('layanans')->insert([
-            'nama_layanan' => 'Surat Keterangan Sehat (KIR)',
-            'harga_layanan' => '20.000'
-            ]);
-
-        DB::table('layanans')->insert([
-            'nama_layanan' => 'Tindakan Injeksi',
-            'harga_layanan' => '15.000'
-            ]);
+        $admin = User::create([
+            'username' => 'admin',
+            'role' => 'admin',
+            'password' => Hash::make('admin'),
+        ]);
+        Admin::create([
+            'user_id' =>  $admin->id,
+            'name' => 'admin account'
+        ]);
+        $dokter = User::create([
+            'username' => 'dokter',
+            'role' => 'dokter',
+            'password' => Hash::make('dokter')
+        ]);
+        Dokter::create([
+            'user_id' => $dokter->id,
+            'nip' => 939993828288245467,
+            'name' => 'Dokter akun',
+            'alamat' => 'alamat si dokter',
+            'bidang' => 'bidang si dokter'
+        ]);
     }
 }
